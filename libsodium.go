5540a89fdde0dba91ec9009c622dc303554d40c0
bengetch <bengetch@gmail.com>
2025-09-10 09:15:57 -0400

    drop windows builds

--- libsodium.go
+++ libsodium.go
@@ -1,9 +1,10 @@
 package rbcl
 
 /*
-#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/prebuilt/darwin-amd64/libsodium.a -framework Security -framework CoreFoundation
-
-#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/prebuilt/darwin-arm64/libsodium.a -framework Security -framework CoreFoundation
+#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/prebuilt/x86_64-apple-darwin/libsodium.a -framework Security -framework CoreFoundation
+#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/prebuilt/arm64-apple-darwin/libsodium.a -framework Security -framework CoreFoundation
+#cgo linux,amd64  LDFLAGS: ${SRCDIR}/prebuilt/x86_64-unknown-linux-gnu/libsodium.a -lm
+#cgo linux,arm64  LDFLAGS: ${SRCDIR}/prebuilt/aarch64-unknown-linux-gnu/libsodium.a -lm
 */
 import "C"
 import "fmt"

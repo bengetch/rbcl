4040bdd43e0e8a789eedf311c2fbf1a45a019b24
bengetch <bengetch@gmail.com>
2025-09-10 08:42:10 -0400

    expand actions to all platforms, add sodium_init() to init function on library

--- libsodium.go
+++ libsodium.go
@@ -6,3 +6,11 @@ package rbcl
 #cgo darwin,arm64 LDFLAGS: ${SRCDIR}/prebuilt/darwin-arm64/libsodium.a -framework Security -framework CoreFoundation
 */
 import "C"
+import "fmt"
+
+func init() {
+	rc := C.sodium_init()
+	if rc < 0 {
+		panic(fmt.Errorf("libsodium initialization failed"))
+	}
+}

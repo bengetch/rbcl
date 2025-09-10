989f33cd402a6b710b0b3bd524810a30223b7642
bengetch <bengetch@gmail.com>
2025-09-10 09:29:10 -0400

    rm old prebuilt dirs, include sodium_init() in libsodium.go

--- libsodium.go
+++ libsodium.go
@@ -5,6 +5,8 @@ package rbcl
 #cgo darwin,arm64 LDFLAGS: ${SRCDIR}/prebuilt/arm64-apple-darwin/libsodium.a -framework Security -framework CoreFoundation
 #cgo linux,amd64  LDFLAGS: ${SRCDIR}/prebuilt/x86_64-unknown-linux-gnu/libsodium.a -lm
 #cgo linux,arm64  LDFLAGS: ${SRCDIR}/prebuilt/aarch64-unknown-linux-gnu/libsodium.a -lm
+
+int sodium_init(void);
 */
 import "C"
 import "fmt"
